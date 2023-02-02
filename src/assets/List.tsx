02b511bc47b9434d6a40145bfac77176317cf595
Kamil Sikora <avler455@gmail.com>
2023-02-02 21:54:07 +0100

    c-01

--- src/assets/List.tsx
+++ src/assets/List.tsx
@@ -1,9 +1,13 @@
-
+import { BrowserRouter , Routes , Route , Link} from "react-router-dom"
 
 export default function List(){
     return (
         <div className="Main-Conteiner">
-            
+            <nav>
+                <BrowserRouter>
+
+                </BrowserRouter>
+            </nav>
         </div>
     )
 }
