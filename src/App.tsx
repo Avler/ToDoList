37a7e61324f58d3455a13b49ea12728e1ac00f4b
Kamil Sikora <avler455@gmail.com>
2023-02-09 21:35:16 +0100

    genarl change

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import List from "./assets/List"
-import Note from "./assets/Note"
+
 import "./App.css"
 import { Routes , Route , Navigate , Link} from "react-router-dom"
+import Home from "./assets/Home"
 
 
 export default function App() {
     return(
         <div className="Cont">
          
-            <List />
-            <Note />
+            
+            
             
             <Routes>
-                    <Route path="/" element={<h1>Hi</h1>}></Route>
+                    <Route path="/" element={<Home />}></Route>
                     <Route path="/day1" element={<h1>1</h1>}></Route>
                     <Route path="/day2" element={<h1>2</h1>}></Route>
                     <Route path="*" element={<Navigate to="/" />}> </Route>
--- src/assets/Home.tsx
+++ src/assets/Home.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import {  Link} from "react-router-dom"
 import {Days} from "./Day1"
+import Home from "./Home"
 
 export default function List(){
     const [selday , setselDay] = useState(Days)
@@ -29,21 +30,33 @@ export default function List(){
         else {
             select = false
         }
-        return elem.id === id ? {...elem , selected:select} : elem
+        return elem.id === id ? {...elem , selected:select , } : {...elem , selected:false}
      }))
     }
 
   const Allday = selday.map(element => { 
+
     return <li className={element.selected ? "day-btn-selected": "day-btn"} onClick={() => sel(element.id)}><Link  to="/day1">{element.name}</Link></li>
    });
     return (
+        <>
         <div className="Main-Conteiner">
-            <nav className="nav">
-                <h1 className="title">To Do List</h1>
-                <ul className="day-links">
-                    {Allday}
-            </ul>
-            </nav>
+            <div className="Main-Conteiner-Nav">
+                <nav className="nav">
+                    <h1 className="title">To Do List</h1>
+                    <ul className="day-links">
+                        {Allday}
+                    </ul>
+                </nav>
+            </div>
+            
+            <div className="home">
+                <p className="home-title">Choose your day</p>
+            </div>
         </div>
+         <div className="Main-Conteiner">
+            
+         </div>
+         </>
     )
 }
